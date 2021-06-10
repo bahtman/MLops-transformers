ea114fd24ff7daa4a877b16f7763405faae8dd78
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-10 17:08:37 +0200

    Predict_model script

---
+++ src/models/predict_model.py
@@ -0,0 +1,24 @@
+import torch
+import matplotlib.pyplot as plt
+accuracy_list = []
+model = torch.load('../../models/model.pth')
+X_test = pd.read_pickle('../../data/processed/X_test.pkl')
+y_test = pd.read_pickle('../../data/processed/y_test.pkl')
+model.eval()
+with torch.no_grad():
+    for images, labels in X_test,y_test:
+        #images, labels = next(iter(testloader))
+        ps = torch.exp(model(images.float()))
+        top_p, top_class = ps.topk(1, dim=1)
+        equals = top_class == labels.view(*top_class.shape)
+        accuracy = torch.mean(equals.type(torch.FloatTensor))
+        accuracy_list.append(accuracy.item()*100)
+    else:
+        print(f'Accuracy: {accuracy.item()*100}%')
+epoch = np.arange(len(accuracy_list))
+print("mean of accuracy = ", np.mean(accuracy_list))
+plt.figure()
+plt.plot(epoch, accuracy_list)
+plt.legend(['Test set accuacy'])
+plt.xlabel('Epochs'), plt.ylabel('Accuacy')
+plt.show()
