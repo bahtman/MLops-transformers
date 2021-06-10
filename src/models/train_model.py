168f362f4688dc2097f303cdd91429b99560c150
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-10 13:22:05 +0200

    train script updates

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,10 @@
 import torch
 import pandas as pd
 from transformers import AutoTokenizer, AutoModel
+from torch import nn, optim
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 train_set = pd.read_pickle('../../data/processed/trainset.pkl')
 test_set = pd.read_pickle('../../data/processed/testset.pkl')
@@ -18,6 +22,34 @@ test_label = test_set['v1']
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModel.from_pretrained("bert-base-uncased")
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+epochs = 5
+loss_list = []
+for e in range(epochs):
+    running_loss = 0
+    for i in range(len(train_data)):
+        optimizer.zero_grad()
+        text = train_data.iloc[i]
+        label = train_label.iloc[1]
+        inputs = tokenizer(text, return_tensors="pt")
+        outputs = model(**inputs)
+        labels = tokenizer(label, return_tensors="pt")
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    else:
+        loss_list.append(running_loss/len(train_data))
+        print(f"Training loss: {running_loss/len(train_data)}")
+plt.figure()
+epoch = np.arange(len(loss_list))
+print(len(loss_list))
+print(epoch)
+plt.plot(epoch, loss_list)
+plt.legend(['Training loss'])
+plt.xlabel('Epochs'), plt.ylabel('Loss')
+plt.show()
+plt.savefig('../../reports/figures/loss_curve')
+torch.save(model, '../../models/model.pth')
 
-inputs = tokenizer("Hello world!", return_tensors="pt")
-outputs = model(**inputs)
