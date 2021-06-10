0fc3404917b0faead019388d48267e86f71974c2
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-10 12:04:43 +0200

    .

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -13,3 +13,12 @@ test_label = test_set['v1']
 
 #trainloader = torch.utils.data.DataLoader(
     #torch.utils.data.TensorDataset(*(train_data,train_label)), batch_size=64, shuffle=True)
+
+
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModel.from_pretrained("bert-base-uncased")
+
+inputs = tokenizer("Hello world!", return_tensors="pt")
+outputs = model(**inputs)
