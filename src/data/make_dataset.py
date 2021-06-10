4a6fe73513a45ef8ec4f0c0a17f6ded22da62017
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-10 11:35:22 +0200

    requirements txt ændringer

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import pickle
-from torchvision import datasets, transforms
 from sklearn.model_selection import train_test_split
 
 def SpamDataSet():
-    print(os.listdir('../')
     data = pd.read_csv('../../data/raw/spam.csv')
     datasubset = data[['v1','v2']]
     trainset, testset = train_test_split(datasubset, test_size=0.33, random_state=42)
