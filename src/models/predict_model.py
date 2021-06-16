9ee2df9e870dd0b4db84e117a004362d5bcbdd2a
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-16 10:29:45 +0200

    :)

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,13 +1,21 @@
 import torch
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 accuracy_list = []
 model = torch.load('../../models/model.pth')
+
 X_test = pd.read_pickle('../../data/processed/X_test.pkl')
 y_test = pd.read_pickle('../../data/processed/y_test.pkl')
+X_test = torch.tensor(X_test)
+y_test = y_test.to_numpy()
+y_test = torch.tensor(y_test)
+testloader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(*(X_test,y_test)), batch_size=64, shuffle=True)
+
 model.eval()
 with torch.no_grad():
-    for images, labels in X_test,y_test:
+    for images, labels in testloader:
         #images, labels = next(iter(testloader))
         ps = torch.exp(model(images.float()))
         top_p, top_class = ps.topk(1, dim=1)
