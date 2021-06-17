fc24db541979aa4e7d88df5292651fb70774837b
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-17 13:13:08 +0200

    timer

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-import wandb
+#import wandb
+import time
 from model import MyAwesomeModel
 from torch import optim
 import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 #wandb.init()
 batch_size = 128
@@ -22,6 +25,8 @@ valloader = torch.utils.data.DataLoader(
             batch_size=batch_size, shuffle=False)
 loss_list = []
 val_loss_list = []
+res = []
+
 print("Training day and night")
 model = MyAwesomeModel()
 #wandb.watch(model, log_freq=500)
@@ -33,8 +38,10 @@ steps = 0
 model.train()
 lowest_val_loss = np.inf
 for e in range(epochs):
+    start = time.time()
     running_loss = 0
     for texts, labels in trainloader:
+        texts, labels = texts.to(device), labels.to(device)
         optimizer.zero_grad()
         output = model(texts)
         output = torch.squeeze(output, 1)
@@ -51,6 +58,7 @@ for e in range(epochs):
         running_loss_val = 0
         model.eval()
         for texts, labels in valloader:
+            texts, labels = texts.to(device), labels.to(device)
             output = model(texts)
             output = torch.squeeze(output,1)
             loss_val = criterion(output.float(), labels.float())
@@ -65,7 +73,11 @@ for e in range(epochs):
         lowest_val_loss = running_loss_val/len(valloader)
     else:
         continue      
-    #wandb.log({"texts": [wandb.Image(i) for i in texts]})
+    #wandb.log({"texts": [wandb.texts(i) for i in texts]})
+    end = time.time()
+    res.append(end - start)
+res = np.array(res)
+print('Timing:', np.mean(res),'+-',np.std(res))
 plt.figure()
 epoch = np.arange(len(loss_list))
 plt.plot(epoch, loss_list)
