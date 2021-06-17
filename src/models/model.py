9b13dfcb11df5e4094e9b84317f98215b12c26d9
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-17 13:57:16 +0200

    ekstra linear lag i model og precision og recall

--- src/models/model.py
+++ src/models/model.py
@@ -7,7 +7,8 @@ class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(768, 256)
-        self.fc3 = nn.Linear(256, 64)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
         self.fc4 = nn.Linear(64, 1)
 
         # Dropout module with 0.2 drop probability
@@ -23,6 +24,7 @@ class MyAwesomeModel(nn.Module):
 
         # Now with dropout
         x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
         x = self.dropout(F.relu(self.fc3(x)))
 
         # output so no dropout here
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -17,14 +17,31 @@ testloader = torch.utils.data.DataLoader(
 
 model.eval()
 with torch.no_grad():
+    tp=0; tn = 0; fp = 0; fn = 0
     for images, labels in testloader:
         # images, labels = next(iter(testloader))
         ps = torch.exp(model(images.float()))
+        #print("ps = ", ps)
         top_p, top_class = ps.topk(1, dim=1)
+        print(len(top_class))
+        #print("top_p = ", top_p, "and top class = ", top_class)
         equals = top_class == labels.view(*top_class.shape)
+        for i in range(len(top_class)):
+            if top_class[i] == 1:
+                if equals[i] == True:
+                    tp += 1
+                if equals[i] == False:
+                    fp += 1
+            if top_class[i] == 0:
+                if equals[i] == True:
+                    tn += 1
+                if equals[i] == False:
+                    fn += 1
+        #print("equals = ", equals)
         accuracy = torch.mean(equals.type(torch.FloatTensor))
         accuracy_list.append(accuracy.item()*100)
 batch_number = np.arange(len(accuracy_list))
+print("tp = ", tp, "fp = ", fp, "tn = ", tn, "fn = ", fn)
 print("mean of accuracy = ", np.mean(accuracy_list))
 plt.figure()
 plt.plot(batch_number, accuracy_list)
